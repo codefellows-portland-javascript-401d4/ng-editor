c67fea71d717562e450ba22f2ef62fa792cc504f
Nathan Pickard <nathan.pickard87@gmail.com>
2016-11-29 07:40:00 -0800

    colorController added to main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 var app = angular.module('myApp', []);
+
+app.controller('colorController', function($scope) {
+  $scope.styles = [
+    { name: 'blue' },
+    { name: 'golden' },
+    { name: 'green'}
+  ];
+  $scope.yourColor = $scope.colors[0];
+});
