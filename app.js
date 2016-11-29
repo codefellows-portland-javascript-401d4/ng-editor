2d2eaa8a3469c409769c4809e2d4a5492ff79675
nsmeds <niilosmeds@gmail.com>
2016-11-28 16:13:19 -0800

    added some colors and fonts

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var app = angular.module('ngApp', []);
 
 app.controller('inputController', function($scope) {
-    $scope.options= ['blue', 'red', 'yellow'];
+    $scope.options = ['black', 'blue', 'red', 'yellow', 'green', 'purple'];
+    $scope.selected = $scope.options[0];
+    $scope.size = 36;
+    $scope.font = 'Andale Mono'
 });
