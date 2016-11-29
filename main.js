b2ba27645137c2770608e442a98c56422f646231
Tim Combs <tim00combs@gmail.com>
2016-11-28 20:08:26 -0800

    add font & color pulldown changes

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 var app =angular.module('myApp', []);
 
 app.controller('someKindaControl', function($scope) {
+  $scope.colors = [
+    {color: 'red'},
+    {color: 'green'},
+    {color: 'blue'}
+  ];
 
+  $scope.fonts = [
+    {font: 'Tillana'},
+    {font: 'Bungee Inline'},
+    {font: 'Bitter'}
+  ];
+
+  $scope.CustomStyle = {};
+  $scope.myColor = $scope.colors[0];
+  $scope.myFont = $scope.fonts[0];
+
+  $scope.selectedColor = '';
+  $scope.selectedFont = '';
+
+  $scope.changeColor = function(option) {
+    $scope.selectedColor = option.color;
+    console.log(option.color);
+  };
+
+  $scope.changeFont = function(option) {
+    $scope.selectedFont = option.font;
+    console.log(option.font);
+  };
 });
