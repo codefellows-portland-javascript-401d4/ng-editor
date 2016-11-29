0faac054459195b3da3eeadf0f0c796cc8a57ed7
gnkatchmar <gunk55@msn.com>
2016-11-28 19:38:12 -0800

    1128-1938 final

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var app = angular.module('myApp', []);
 
 app.controller('formCtrl', function($scope) {
-  $scope.message = 'Type Message Here';
-  $scope.fonts = ['Helvetica', 'Times New Roman', 'Verdana'];
+  $scope.fonts = ['Courier New', 'Helvetica', 'Impact', 'Times New Roman', 'Verdana'];
 });
