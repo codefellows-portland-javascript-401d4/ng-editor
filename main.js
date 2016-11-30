cddac84bc5a089801e17fb00dcaa258c38fe2590
mizutombo <thomas.shultz52@gmail.com>
2016-11-29 17:19:53 -0800

    debugged code ... confirmed full functionality

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
 var myApp = angular.module('ngEditor', []); // eslint-disable-line
 
-myApp.controller('fontFamily', ['$scope', function ($scope) {
-  $scope.family = {
-    name: 'font family'
-  };
-}]);
-
-myApp.controller('fontColor', ['$scope', function ($scope) {
+myApp.controller('main', ['$scope', function ($scope) {
+  $scope.message = 'hello world';
+  $scope.family = { name: 'font family' };
   $scope.colors = [
     {name: 'black'},
     {name: 'red'},
     {name: 'blue'},
     {name: 'green'}
   ];
-  $scope.myColor = $scope.colors[2];
-}]);
-
-myApp.controller('fontSize', ['$scope', function ($scope) {
   $scope.value = 12;
   $scope.min = 8;
   $scope.max = 48;
